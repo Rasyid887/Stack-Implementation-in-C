c03be6497ce98ebf7443bfbfbb264c4cfae7879a
Aulia Rasyid Pratama <rasyidpratama23@gmail.com>
2025-12-03 12:56:53 +0700

    feat: add push, pop, and peek feature

--- src/main.c
+++ src/main.c
@@ -18,6 +18,9 @@ Stack *stack_init(int capacity);
 bool   stack_delete(Stack *stack);
 bool   is_full(Stack *stack);
 bool   is_empty(Stack *stack);
+bool   stack_push(Stack *stack, int data);
+bool   stack_peek(Stack *stack, int *data);
+bool   stack_pop(Stack *stack, int *data);
 
 int main() {
     Stack *stack = stack_init(5);
@@ -31,7 +34,27 @@ int main() {
         puts("stack is empty");
     }
 
-    stack_delete(stack);
+    stack_push(stack, 5);
+
+    if (!is_empty(stack)) {
+        puts("stack is not empty");
+    }
+
+    stack_push(stack, 6);
+    stack_push(stack, 7);
+    stack_push(stack, 8);
+
+    printf("Stack size : %d\n", stack->size);
+
+    int data  = 0;
+    int data2 = 0;
+
+    stack_pop(stack, &data);
+    stack_peek(stack, &data2);
+
+    printf("%d\n", data2);
+
+    // stack_delete(stack);
 
     return 0;
 }
@@ -73,4 +96,36 @@ bool is_full(Stack *stack) {
 
 bool is_empty(Stack *stack) {
     return stack->size == 0;
+}
+
+bool stack_push(Stack *stack, int data) {
+    if (is_full(stack)) {
+        return false;
+    }
+
+    stack->collection[stack->size] = data;
+    stack->size++;
+
+    return true;
+}
+
+bool stack_peek(Stack *stack, int *data) {
+    if (is_empty(stack)) {
+        return false;
+    }
+
+    *data = stack->collection[stack->size - 1];
+
+    return true;
+}
+
+bool stack_pop(Stack *stack, int *data) {
+    if (is_empty(stack)) {
+        return false;
+    }
+
+    stack->size--;
+    *data = stack->collection[stack->size];
+
+    return true;
 }
